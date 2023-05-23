0518eea099cf9579173daa0e3f2fcd248f727e4f
Yury Bandeira <yurybs@gmail.com>
2023-05-23 17:23:46 -0400

    Correcting lint

--- app/app/test.py
+++ app/app/test.py
@@ -5,17 +5,18 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
-    
+        self.assertEqual(res, 11)
+
     def test_subctract_numbers(self):
         """Test subtracting numbers."""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
